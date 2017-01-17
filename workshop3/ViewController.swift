ce545831801939c87c42df2ddfc35bb7989f15bb
Wei Fan <weifan@Fanweis-MacBook-Pro.local>
2017-01-17 10:20:09 +0800

    create project

---
+++ workshop3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  workshop3
+//
+//  Created by Wei Fan on 1/17/17.
+//  Copyright © 2017 Wei Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
